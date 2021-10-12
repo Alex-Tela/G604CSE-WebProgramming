6cbc1ad5d2e92f84790e174d5beb3dd8f34e56bb
Uke Alex <telasoftware.contact@gmail.com>
2021-10-12 12:13:12 +0300

    Adding Fetch API

--- Login-Page/actions.js
+++ Login-Page/actions.js
@@ -1,16 +1,18 @@
 const buttons = document.querySelectorAll('button');
 const inputs = document.querySelectorAll('input');
+const warning = document.getElementById('warning');
 let clicks = 0;
 
 buttons[0].addEventListener('click', () => {
     clicks++;
     if (clicks === 1) {
-        console.log("Warning! You are about to reset your inputs. Are you sure you want to continue?");
+        warning.style.display = "block";
     } else if (clicks > 1) {
         inputs.forEach(input => {
             input.value = "";
         })
         clicks = 0;
+        warning.style.display = "none";
     }
     console.log(clicks);
 });
@@ -25,11 +27,25 @@ buttons[1].addEventListener('click', () => {
 
     console.log(check);
     if (check < 1) {
-        console.log("Congratulations! You have successfully created an account!");
-        let sum = 0;
-        for (let i = 0; i <= 50; i++) {
-            sum += i;
-        }
-        console.log(`Sum of numbers up to 50: ${sum}`)
+        const data = {
+            name: document.getElementById('name').value,
+            email: document.getElementById('email').value,
+            password: document.getElementById('password').value,
+            confirm: document.getElementById('confirm').value
+        } 
+
+        const response = fetch("", {
+            method: "POST",
+            body: JSON.stringify(data)
+        }).then((r) => {
+            console.log("Congratulations! You have successfully created an account!\n" + JSON.stringify(data))})
+        .catch((r) => {
+            console.log("Congratulations! You have successfully created an account!\n" + JSON.stringify(data))});
+    
+        // let sum = 0;
+        // for (let i = 0; i <= 50; i++) {
+        //     sum += i;
+        // }
+        // console.log(`Sum of numbers up to 50: ${sum}`)
     }
 });
